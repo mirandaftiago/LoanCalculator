d0f0e97446e02c320e100ebbcbc9facd947fa27e
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-15 00:42:02 +0000

    added error logic

--- app.js
+++ app.js
@@ -25,7 +25,32 @@ function calculateResults(e){
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - loanAmount).toFixed(2);
   } else {
-    console.log('Please check your numbers');
+    showError('Please fill out all the fields');
   }
   e.preventDefault();
+}
+
+//Show error
+function showError(error){
+  //Create a div
+  const errorDiv = document.createElement('div');
+
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  //Add class
+  errorDiv.className = 'alert alert-danger';
+
+  //Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  //Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  //Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+function clearError(){
+  document.querySelector('.alert').remove();
 }
