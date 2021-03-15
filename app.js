4328aeedfada375fa7d2d1d8cafdedb1fe2eea4c
Tiago Miranda <miranda.f.tiago@gmail.com>
2021-03-15 00:33:34 +0000

    added monthly payments logic

--- app.js
+++ app.js
@@ -8,12 +8,24 @@ function calculateResults(e){
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthlyPayment');
-  const totalPayment = document.getElementById('totalPayment');
-  const totalInterest = document.getElementById('totalInterest');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
 
   const loanAmount = parseFloat(amount.value);
   const calculatedInterest = parseFloat(interest.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
+
+  //Calculate monthly payment
+  const payments = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (loanAmount * payments * calculatedInterest)/(payments - 1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - loanAmount).toFixed(2);
+  } else {
+    console.log('Please check your numbers');
+  }
   e.preventDefault();
 }
